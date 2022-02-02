59df16062457082ed4c5d7af41753fed9a2b9438
shivam <srvcode@gmail.com>
2022-02-02 19:39:38 +0530

    fifth commit

--- src/com/prepcoding/ds/level1/basic/functionArrays/AnyBaseToDecimalDemo.java
+++ src/com/prepcoding/ds/level1/basic/functionArrays/AnyBaseToDecimalDemo.java
@@ -4,14 +4,33 @@ import java.util.Scanner;
 
 /*
  * Question: Given a number 'n' & its base 'b' & we're required to convert the number 'n' into a decimal number.
+ * 
+ * 1. Time Complexity: O(log10 n)
+ * 2. Space Complexity: O(1)
  */
 
 public class AnyBaseToDecimalDemo {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int b = scan.nextInt();
 		
+		int decimalNo = getDecimalNumber(n, b);
+		System.out.println(decimalNo);
 		scan.close();
+	}
+
+	private static int getDecimalNumber(int n, int b) {
+		int ans = 0;
+		int count = 0;
+		while(n != 0) {
+			int rem = n % 10;
+			ans += rem * Math.pow(b, count);
+			count++;
+			n = n/10;
+		}
+		return ans;
 	}
 
 }
